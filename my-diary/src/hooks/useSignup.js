362a16647a572f6d2f6b51230a95da734509c4b9
EUNCHAE KIM <112812483+eunbbori@users.noreply.github.com>
2023-02-21 17:54:20 +0900

    - 기본 페이지 라우팅 - 네이게이션 컴포넌트 연결 - Login.js 에 폼 컨트롤 추가 - Firebase 환경설정 - Firebase Authentication 설정 - 회원가입 useSignup 훅 적용

---
+++ my-diary/src/hooks/useSignup.js
@@ -0,0 +1,37 @@
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { useState } from "react";
+import { appAuth } from "../firebase/config";
+
+export const useSignup = () => {
+  const [error,setError] = useState(null); //에러 정보
+  const [isPending,setIsPending] = useState(false); //현재 서버와 통신 상태 
+
+  const signup = (email,password,displayName) => {
+    setError(null); 
+    setIsPending(true);
+
+    createUserWithEmailAndPassword(appAuth,email,password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log(user);
+
+      if(!user) {
+        throw new Error("회원가입에 실패했습니다.");
+      }
+
+      updateProfile(appAuth.currentUser,{displayName})
+      .then(()=> {
+        setError(null);
+        setIsPending(false);
+      }).catch((err)=>{
+        setError(err.message);
+        setIsPending(false);
+      });
+    }).catch((err) => {
+      setError(err.message);
+      setIsPending(false);
+    });
+  }
+  return{error,isPending,signup}
+
+}
